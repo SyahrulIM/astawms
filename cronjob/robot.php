e66ac6c51a187e669c51f4e4cfa4896ccb3d59b8
Syahrul Izha <74164767+SyahrulIM@users.noreply.github.com>
2025-11-19 14:07:25 +0700

    update add cronjob

---
+++ cronjob/robot.php
@@ -0,0 +1,23 @@
+<?php
+
+$token = 'EyuhsmTqzeKaDknoxdxt';
+$message = 'Test';
+$target = '85156340619';
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.fonnte.com/send',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => array(
+        'target' => $target,
+        'message' => $message,
+        'countryCode' => '62',
+    ),
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: ' . $token
+    ),
+));
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response;
